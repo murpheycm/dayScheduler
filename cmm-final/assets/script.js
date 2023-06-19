aee2217ee55df80f56163567caf15fbd380bc239
murpheycm <cmmurphey@gmail.com>
2023-06-19 15:05:31 -0400

    Updated time block variables and time

--- cmm-final/assets/script.js
+++ cmm-final/assets/script.js
@@ -1,43 +1,92 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
+$(document).ready(function() {
+
+  //Function to create date
+  var currentTime = dayjs().format("dddd, MMMM DD YYYY");
+  $("#currentDay").text(currentTime);
+
+
+  // colorcode timeblocks to indicate whether they are in the past, present or future
+     var currentHour = dayjs().format('H');
+     $(".time-block").each(function() {
+         if (parseInt(currentHour) === parseInt(this.id)) {
+             $(this).addClass("present");
+         } else if (parseInt(currentHour) > parseInt(this.id)) {
+             $(this).addClass("past");
+         } else {
+             $(this).addClass("future");
+         }
+      });
+      $(".saveBtn").each(function() {
+        if (parseInt(currentHour) === parseInt(this.id.split("-")[1])) {
+            $(this).addClass("present");
+        } else if (parseInt(currentHour) > parseInt(this.id.split("-")[1])) {
+            $(this).addClass("past");
+        } else {
+            $(this).addClass("future");
+        }
+    })
+
+    // client-side storage for events
+    var storedEvents = [];
+
+    initialise();
+
+    function renderEvents() {
+        $("textarea").each(function() {
+            this.value = "";
+        })
+        
+        $.each(storedEvents, function() {
+            $("textarea" + this.eventTime)[0].value = this.eventText;
+        }) 
+    }
+
+    function initialise() {
+        var userEvent = JSON.parse(localStorage.getItem("storedEvents"));
+        if (userEvent !== null) {
+            storedEvents = userEvent;
+        }
+
+        renderEvents();
+    }
+
+    function storeEvents() {
+        localStorage.setItem("storedEvents", JSON.stringify(storedEvents));
+    }
+
+    $("button").on("click", function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var className = $(event.target).attr("class");
+        
+        var eventObject = {
+            eventTime: className,
+            eventText: $("textarea." + className).val()
+        } 
+        console.log(eventObject.eventText);
+
+        if (storedEvents.length > 0) {
+            $.each(storedEvents, function() {
+                if (this.eventTime === event.target.className) {
+                    storedEvents.splice($.inArray(this, storedEvents), 1);
+                }    
+            });
+        }
+
+        storedEvents.push(eventObject);
+
+        $.each(storedEvents, function() {
+            if (this.eventText === "") {
+                storedEvents.splice($.inArray(this, storedEvents), 1);
+            }
+        })
+
+        storeEvents();
+        renderEvents();
+    })
 
 
 
 
-
-
-
-    
-    // TODO: Add code to display the current date in the header of the page.
- 
-    const currentDate = new Date();
-    // Get the day of the week (0-6)
-    const dayOfWeek = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
-    // Get the month of the year
-    const monthOfYear = currentDate.toLocaleDateString('en-US', { month: 'long' });
-    // Get the date (1-31)
-    const date = currentDate.getDate();
-    // Format the current day and date string
-    const currentDayAndDate = `${dayOfWeek}, ${monthOfYear} ${date}`;
-    // Display current date on the page under the header
-    document.getElementById("currentDay").textContent = currentDayAndDate;
-  });
-  
+});
