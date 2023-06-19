459a01fb09d73abcdd94ccbde67234edff17d368
murpheycm <cmmurphey@gmail.com>
2023-06-19 15:13:12 -0400

    Updates to work towards defining time blocks in JS instead of html.

--- cmm-final/assets/script.js
+++ cmm-final/assets/script.js
@@ -4,89 +4,98 @@ $(document).ready(function() {
   var currentTime = dayjs().format("dddd, MMMM DD YYYY");
   $("#currentDay").text(currentTime);
 
+  const timeBlock = [
+    {
+      label: "9:00 AM",
+      tValue: "9",
+      userInput: "",
+    },
+    {
+      label: "10:00 AM",
+      tValue: "10",
+      userInput: "",
+    },
+    {
+      label: "11:00 AM",
+      tValue: "11",
+      userInput: "",
+    },
+    {
+      label: "12:00 PM",
+      tValue: "12",
+      userInput: "",
+    },
+    {
+      label: "1:00 PM",
+      tValue: "13",
+      userInput: "",
+    },
+    {
+      label: "2:00 PM",
+      tValue: "14",
+      userInput: "",
+    },
+    {
+      label: "3:00 PM",
+      tValue: "15",
+      userInput: "",
+    },
+    {
+      label: "4:00 PM",
+      tValue: "16",
+      userInput: "",
+    },
+    {
+      label: "5:00 PM",
+      tValue: "17",
+      userInput: "",
+    },
+  ];
 
-  // colorcode timeblocks to indicate whether they are in the past, present or future
-     var currentHour = dayjs().format('H');
-     $(".time-block").each(function() {
-         if (parseInt(currentHour) === parseInt(this.id)) {
-             $(this).addClass("present");
-         } else if (parseInt(currentHour) > parseInt(this.id)) {
-             $(this).addClass("past");
-         } else {
-             $(this).addClass("future");
-         }
-      });
-      $(".saveBtn").each(function() {
-        if (parseInt(currentHour) === parseInt(this.id.split("-")[1])) {
-            $(this).addClass("present");
-        } else if (parseInt(currentHour) > parseInt(this.id.split("-")[1])) {
-            $(this).addClass("past");
-        } else {
-            $(this).addClass("future");
-        }
-    })
-
-    // client-side storage for events
-    var storedEvents = [];
-
-    initialise();
-
-    function renderEvents() {
-        $("textarea").each(function() {
-            this.value = "";
-        })
-        
-        $.each(storedEvents, function() {
-            $("textarea" + this.eventTime)[0].value = this.eventText;
-        }) 
+  // Rows
+  $(timeBlock).each(function (i) {
+    const row = $("<div>");
+    if (i < $(timeBlock).length) {
+      row
+        .addClass("row")
+      $(".container").append(row);
     }
+    i++
+  });
 
-    function initialise() {
-        var userEvent = JSON.parse(localStorage.getItem("storedEvents"));
-        if (userEvent !== null) {
-            storedEvents = userEvent;
-        }
+  // Columns with hours
+  $("div.row").each(function (i) {
+    const timeValue = timeBlock[i].tValue;
+    const labelCol = $("<div>");
+    const inputCol = $("<div>");
 
-        renderEvents();
+    labelCol
+      .addClass("col-2 hour")
+      .text(timeBlock[i].label)
+    inputCol
+      .addClass("col-10 time-block")
+      .attr("value", timeValue)
+    $(this).append(labelCol);
+    $(this).append(inputCol);
+    i++
+  });
+
+
+  // Past, Present, or Future time blocks in relation to the current time
+  $(".time-block").each(function (i) {
+    const currentHour = parseInt(dayjs().format('H'));
+    const timeId = $(this).attr("value");
+    if (currentHour < timeId) {
+      $(this).addClass("future");
+    } else if (currentHour == timeId) {
+      $(this).addClass("present");
+    } else if (currentHour > timeId) {
+      $(this).addClass("past");
     }
+    i++
+  })
 
-    function storeEvents() {
-        localStorage.setItem("storedEvents", JSON.stringify(storedEvents));
-    }
+  
 
-    $("button").on("click", function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-
-        var className = $(event.target).attr("class");
-        
-        var eventObject = {
-            eventTime: className,
-            eventText: $("textarea." + className).val()
-        } 
-        console.log(eventObject.eventText);
-
-        if (storedEvents.length > 0) {
-            $.each(storedEvents, function() {
-                if (this.eventTime === event.target.className) {
-                    storedEvents.splice($.inArray(this, storedEvents), 1);
-                }    
-            });
-        }
-
-        storedEvents.push(eventObject);
-
-        $.each(storedEvents, function() {
-            if (this.eventText === "") {
-                storedEvents.splice($.inArray(this, storedEvents), 1);
-            }
-        })
-
-        storeEvents();
-        renderEvents();
-    })
-
-
-
-
+  
 });
